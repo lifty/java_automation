703676facd356680d65a67a641d31c4e57678f82
Ksenia <liftykun@gmail.com>
2021-10-04 21:57:10 +0300

    Ex 5, 6
    Complete ex 5 (testSaveTwoArticles) and 6 (testCheckArticleTitle)

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,16 +1,19 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.net.URL;
+import java.util.List;
 
 public class FirstTest {
 
@@ -119,6 +122,127 @@ public class FirstTest {
         }
     }
 
+    @Test
+    public void testSwipeUpSearchResults() throws InterruptedException
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Skip')]"),
+                "Cannot find Skip element",
+                5);
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5);
+        String input_key = "Appium";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find Search field element",
+                5,
+                input_key);
+        int element_sum = driver.findElements(By.id("org.wikipedia:id/page_list_item_title")).size();
+        boolean compare_counted_elements = element_sum > 0;
+        assert compare_counted_elements: "The number of articles is " + element_sum +", we are expecting at least 1";
+        swipeUpToFindElement(
+                By.xpath("//*[@text='RTTS']"), "Cannot find RTTS article", 3);
+    }
+
+    @Test
+    public void testSaveTwoArticles() throws InterruptedException
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Skip')]"),
+                "Cannot find Skip element",
+                5);
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5);
+        String input_key = "Java";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find Search field element",
+                5,
+                input_key);
+        waitForElementAndClick(
+                By.xpath("//*[@text='Object-oriented programming language']"),
+                "Cannot find searched article",
+                5);
+        waitForElementAndClick(
+                By.xpath("//android.widget.TextView[@text='Save']"),
+                "Cannot find Save button",
+                5);
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot find Back button",
+                5);
+        waitForElementAndClick(
+                By.xpath("//*[@text='Indonesian island']"),
+                "Cannot find searched article",
+                5);
+        waitForElementAndClick(
+                By.xpath("//android.widget.TextView[@text='Save']"),
+                "Cannot find Save button",
+                5);
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot find Back button",
+                5);
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@index=0]"),
+                "Cannot find Back button",
+                5);
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='Saved']"),
+                "Cannot find Saved button",
+                5);
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/item_reading_list_statistical_description' and contains(@text,'2 articles')]"),
+                "Cannot find a list with 2 articles",
+                5);
+        swipeElementToLeft(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_description' and @text='Object-oriented programming language']"),
+                "Cannot find saved article");
+        waitForElementNotPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_description' and @text='Object-oriented programming language']"),
+                "Cannot delete saved article",
+                5);
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_description' and @text='Indonesian island']"),
+                "Cannot find and click the article that should stay in the list",
+                5);
+        waitForElementPresent(By.xpath("//android.webkit.WebView[@content-desc='Java']//*[contains(@content-desc,'Javanese')]"),
+                "Cannot prove the article is the expected one",
+                5);
+
+    }
+
+    @Test
+    public void testCheckArticleTitle() throws InterruptedException
+    {
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Skip')]"),
+                "Cannot find Skip element",
+                5);
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5);
+        String input_key = "Java";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find Search field element",
+                5,
+                input_key);
+        waitForElementAndClick(
+                By.xpath("//*[@text='Object-oriented programming language']"),
+                "Cannot find searched article",
+                5);
+        assertElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find the title of the article");
+    }
+
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
     {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
@@ -140,6 +264,7 @@ public class FirstTest {
     {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSecond);
         element.sendKeys(keys);
+        driver.hideKeyboard();
         Thread.sleep(1000);
         return element;
     }
@@ -167,4 +292,84 @@ public class FirstTest {
                 ExpectedConditions.invisibilityOfElementLocated(by)
         );
     }
+
+    protected void swipeUp(int timeOfSwipe)
+    {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width / 2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+    }
+
+    protected void swipeUpQuick()
+    {
+        swipeUp(200);
+    }
+
+    protected void swipeUpToFindElement(By by, String error_message, int max_swipes) throws InterruptedException
+    {
+        int already_swiped = 0;
+        while (driver.findElements(by).size() == 0)
+        {
+            if (already_swiped > max_swipes)
+            {
+                waitForElementPresent(by, "Cannot find element by swiping up. \n" + error_message, 0);
+                return;
+            }
+            swipeUpQuick();
+            Thread.sleep(1000);
+            ++already_swiped;
+        }
+    }
+
+    protected void swipeElementToLeft(By by, String error_message)
+    {
+        WebElement element = waitForElementPresent(by, error_message, 10);
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int y = (upper_y + lower_y) / 2;
+        TouchAction action = new TouchAction(driver);
+        action.press(right_x, y).waitAction(150).moveTo(left_x, y).release().perform();
+    }
+
+    private int getAmountOfElements(By by)
+    {
+        List elements = driver.findElements(by);
+        return elements.size();
+    }
+
+    private void assertElementPresent(By by, String error_message)
+    {
+        int amount_of_elements = getAmountOfElements(by);
+        if (amount_of_elements == 0) {
+            String default_message = "An element '" + by.toString() + "' supposed to be presented";
+            throw new AssertionError(default_message + "\n" + error_message);
+        }
+    }
+//
+//    private void assertElementNotPresent(By by, String error_message)
+//    {
+//        int amount_of_elements = getAmountOfElements(by);
+//        if (amount_of_elements > 0) {
+//            String default_message = "An element '" + by.toString() + "' supposed to be not presented";
+//            throw new AssertionError(default_message + " " + error_message);
+//        }
+//    }
+//
+//    private void rotateScreenLS()
+//    {
+//        driver.rotate(ScreenOrientation.LANDSCAPE);
+//        return;
+//    }
+//
+//    private void rotateScreenPT()
+//    {
+//        driver.rotate(ScreenOrientation.PORTRAIT);
+//        //driver.runAppInBackground(5);
+//        return;
+//    }
 }
